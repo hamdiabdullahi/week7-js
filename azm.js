e58a5f20a9af9b0bcd9d9675d996af50b35666aa
student <abdullahihamdiabass@gmail.com>
2021-04-05 06:55:09 +0300

    hamdi

---
+++ azm.js
@@ -0,0 +1,26 @@
+
+// for ...in  loop
+const names=["Aisha","Miana","Florence","Neonka","Azmko"]
+for(const k of names){
+    console.log(k);
+};
+
+const symbols={
+    aa:"Aisha",
+    ma:"Miana",
+    fe:"Florence",
+    ao:"Azmko"
+
+};
+for(const k in symbols){
+    console.log(`${k}:${"symbols"}`);
+};
+
+//for....of loop
+const countriesarray4 = ["Kenya","Uganda","Rwanda"];
+for (const m of countriesarray4){
+    console.log(m);
+
+}
+
+
